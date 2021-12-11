1fa72fc1b37cf786a5f5e5153f3548df0854d30f
fatehmahal1992 <fatehmahal1992@gmail.com>
2021-12-10 17:47:55 -0700

    first comment

---
+++ src/com/java/basics/Print_Statements.java
@@ -0,0 +1,23 @@
+package com.java.basics;
+
+public class Print_Statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		for (int i=1;i<=9;i++)
+		{
+		System.out.println();
+		
+		for (int j=1; j<=5; j++) 
+		{
+		System.out.print(" 1");
+		
+	
+		}	
+	
+			}
+		}
+	
+	}
+
